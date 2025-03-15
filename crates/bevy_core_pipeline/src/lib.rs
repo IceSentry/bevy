c30b56fae8b2922b0fd6d6b4e1fcc5851363fb3c
IceSentry <c.giguere42@gmail.com>
2025-03-15 03:09:06 -0400

    wip

--- crates/bevy_core_pipeline/src/lib.rs
+++ crates/bevy_core_pipeline/src/lib.rs
@@ -8,14 +8,11 @@
 
 pub mod auto_exposure;
 pub mod blit;
-pub mod bloom;
 pub mod core_2d;
 pub mod core_3d;
 pub mod deferred;
-pub mod dof;
 pub mod experimental;
 pub mod fullscreen_vertex_shader;
-pub mod motion_blur;
 pub mod msaa_writeback;
 pub mod oit;
 pub mod post_process;
@@ -36,14 +33,11 @@ pub mod prelude {
 
 use crate::{
     blit::BlitPlugin,
-    bloom::BloomPlugin,
     core_2d::Core2dPlugin,
     core_3d::Core3dPlugin,
     deferred::copy_lighting_id::CopyDeferredLightingIdPlugin,
-    dof::DepthOfFieldPlugin,
     experimental::mip_generation::MipGenerationPlugin,
     fullscreen_vertex_shader::FULLSCREEN_SHADER_HANDLE,
-    motion_blur::MotionBlurPlugin,
     msaa_writeback::MsaaWritebackPlugin,
     post_process::PostProcessingPlugin,
     prepass::{DeferredPrepass, DepthPrepass, MotionVectorPrepass, NormalPrepass},
@@ -77,9 +71,6 @@ impl Plugin for CorePipelinePlugin {
                 MsaaWritebackPlugin,
                 TonemappingPlugin,
                 UpscalingPlugin,
-                BloomPlugin,
-                MotionBlurPlugin,
-                DepthOfFieldPlugin,
                 PostProcessingPlugin,
                 OrderIndependentTransparencyPlugin,
                 MipGenerationPlugin,
